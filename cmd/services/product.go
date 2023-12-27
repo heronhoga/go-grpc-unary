60d06352a542bd4a319a5f8306df5a837338d1e5
heronhoga <nozzeravixer@gmail.com>
2023-12-27 13:59:48 +0700

    feat:add getProducts method

--- cmd/services/product.go
+++ cmd/services/product.go
@@ -1,8 +1,45 @@
 package services
 
 import (
+	"context"
 	productPb "go-grpc-unary/pb/product"
+	pagingPb "go-grpc-unary/pb/pagination"
 )
 type ProductService struct {
 	productPb.UnimplementedProductServiceServer
+}
+
+func (p *ProductService) GetProducts(context.Context, *productPb.Empty) (*productPb.Products, error) {
+	products := &productPb.Products{
+		Pagination: &pagingPb.Pagination{
+			Total: 10,
+			PerPage: 4,
+			CurrentPage: 1,
+			LastPage: 2,
+		},
+		Data: []*productPb.Product{
+			{
+				Id: 1,
+				Name: "Nike Tees",
+				Price: 10000,
+				Stock: 10,
+				Category: &productPb.Category{
+					Id: 1,
+					Name: "shirt",
+				},
+			},
+			{
+				Id: 2,
+				Name: "Adidas Tees",
+				Price: 120000,
+				Stock: 3,
+				Category: &productPb.Category{
+					Id: 1,
+					Name: "shirt",
+				},
+			},
+		},
+	}
+
+	return products, nil
 }
