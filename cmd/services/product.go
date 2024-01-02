2dfd39c86a696fb5df044f78cca31b34d9bd12ff
heronhoga <nozzeravixer@gmail.com>
2024-01-02 21:44:38 +0700

    feat:add get specific product method

--- cmd/services/product.go
+++ cmd/services/product.go
@@ -63,3 +63,22 @@ func (p *ProductService) GetProducts(ctx context.Context,pageParam *productPb.Pa
 	return response, nil
 
 }
+
+func (p *ProductService) GetProduct(ctx context.Context, id *productPb.Id) (*productPb.Product, error) {
+	row := p.DB.Table("products as p").
+	Joins("INNER JOIN categories AS c on c.id = p.category_id").
+	Select("p.id", "p.name", "p.price", "p.stock", "c.id", "c.name").
+	Where("p.id = ?", id.GetId()).
+	Row()
+
+	var product productPb.Product
+	var category productPb.Category
+
+	if err := row.Scan(&product.Id, &product.Name, &product.Price, &product.Stock, &category.Id, &category.Name); err != nil {
+		log.Fatal("error getting data")
+	}
+
+	product.Category = &category
+	
+	return &product, nil
+}
